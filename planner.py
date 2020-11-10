48da87926f07486210abce43d9dc5d2f15a2b9ba
diio <dimaionov95@mail.ru>
2020-11-10 16:09:45 +0200

    doreload

---
+++ planner.py
@@ -0,0 +1,393 @@
+from megamap import A
+import math
+
+
+def create_zero_matrix(x, y):
+    A = [[0] * x for n in range(y)]
+    return A
+
+
+def get_line(x_start, y_start, x_finish, y_finish):
+    if (x_finish - x_start) == 0:
+        k = (y_finish - y_start) / 1
+        b = y_start - (k * x_start)
+        return k, b
+    k = (y_finish - y_start) / (x_finish - x_start)
+    b = y_start - (k * x_start)
+    return k, b
+
+
+def end_of_line(A_map, x_start, y_start, x_finish, y_finish, transon=1):
+    k, b = get_line(x_start, y_start, x_finish, y_finish)
+    x_stop, y_stop = math.trunc(x_finish), math.trunc(y_finish)
+    if abs(x_start - x_finish) < abs(y_finish - y_start):
+        for y in range(y_start, y_finish):
+            x = (y - b) / k
+            if A_map[y][math.trunc(x)] == transon:
+                y_stop = y
+                x_stop = math.trunc(x)
+                return x_stop, y_stop
+    if abs(x_start - x_finish) >= abs(y_finish - y_start):
+        for x in range(x_start, x_finish):
+            y = k * x + b
+            if A_map[math.trunc(y)][x] == transon:
+                y_stop = math.trunc(y)
+                x_stop = x
+                return x_stop, y_stop
+
+
+# неиспользуеемая функция вычета массивов
+# def SS_min_RR(x, y, SS, RR):
+#     TS = create_zero(x, y)
+#     for stroka in range(len(SS)):
+#         for number in range(len(SS[stroka])):
+#             elemS = SS[stroka][number]
+#             elemR = RR[stroka][number]
+#             if elemS == 1:
+#                 if elemR == 1:
+#                     TS[stroka][number] = 0
+#                 if elemR == 0:
+#                     TS[stroka][number] = 1
+#             if elemS == 0:
+#                 TS[stroka][number] = 0
+#     return TS
+
+
+def create_filled_contour(x_stop, y_stop, A_map):  # simple deep filter
+    x_edge, y_edge = len(A_map[1]), len(A_map)
+    OBT = create_zero_matrix(x_edge, y_edge)
+    OBT[y_stop][x_stop] = 1
+    nums = 1
+    k = nums - 1
+    while k != nums:
+        k = nums
+        for stroka in range(len(OBT)):
+            for number in range(len(OBT[stroka])):
+                if OBT[stroka][number] == 1:
+
+                    if (0 <= stroka < y_edge) and (0 <= number + 1 < x_edge):
+                        if A_map[stroka][number + 1] != 0 and OBT[stroka][number + 1] != 1:  # right
+                            OBT[stroka][number + 1] = 1
+                            nums += 1
+
+                    if (0 <= stroka < y_edge) and (0 <= number - 1 < x_edge):
+                        if A_map[stroka][number - 1] != 0 and OBT[stroka][number - 1] != 1:  # left
+                            OBT[stroka][number - 1] = 1
+                            nums += 1
+
+                    if (0 <= stroka - 1 < y_edge) and (0 <= number < x_edge):
+                        if A_map[stroka - 1][number] != 0 and OBT[stroka - 1][number] != 1:  # up
+                            OBT[stroka - 1][number] = 1
+                            nums += 1
+
+                    if (0 <= stroka + 1 < y_edge) and (0 <= number < x_edge):
+                        if A_map[stroka + 1][number] != 0 and OBT[stroka + 1][number] != 1:  # down
+                            OBT[stroka + 1][number] = 1
+                            nums += 1
+    return OBT  # nums could be added as needed
+
+
+def create_1st_S(MAP1):
+    x, y = len(MAP1[1]), len(MAP1)
+    MAP2 = create_zero_matrix(x, y)
+    for stroka in range(len(MAP1)):
+        for number in range(len(MAP1[stroka])):
+            elem0 = MAP1[stroka][number]
+            if elem0 == 1:
+                MAP2[stroka][number] = elem0
+                if (0 <= stroka < y) and (0 <= number + 1 < x):  # right
+                    MAP2[stroka][number + 1] = 1
+                if (0 <= stroka < y) and (0 <= number - 1 < x):  # left
+                    MAP2[stroka][number - 1] = 1
+                if (0 <= stroka - 1 < y) and (0 <= number < x):  # up
+                    MAP2[stroka - 1][number] = 1
+                if (0 <= stroka + 1 < y) and (0 <= number < x):  # down
+                    MAP2[stroka + 1][number] = 1
+    return MAP2
+
+
+def find_center(B_map):
+    sum_x, sum_y = 0, 0
+    num = 0
+    for stroka in range(len(B_map)):
+        for number in range(len(B_map[stroka])):
+            if B_map[stroka][number] == 1:
+                sum_x = sum_x + number
+                sum_y = sum_y + stroka
+                num += 1
+    x_center = sum_x / num
+    y_center = sum_y / num
+    return x_center, y_center
+
+
+def find_rad_obtacle(x_center, y_center, B_map):
+    r_max = 0
+    for y in range(len(B_map)):
+        for x in range(len(B_map[y])):
+            if B_map[y][x] == 1:
+                R_iteration = math.sqrt((x - x_center) * (x - x_center) + (y - y_center) * (y - y_center))
+                if R_iteration > r_max:
+                    r_max = math.ceil(R_iteration)
+    return r_max
+
+
+def find_alfa(x_stop, y_stop, x_center, y_center):
+    gip = math.sqrt((x_center - x_stop) * (x_center - x_stop) + (y_stop - y_center) * (y_stop - y_center))
+    alfa = math.asin((x_center - x_stop) / gip)
+    alfa_grad = alfa * 57.3
+    alfa_start = 0
+    if x_stop <= x_center:
+        if alfa_grad >= 0:
+            alfa_start = alfa_grad
+            return alfa_start
+        alfa_start = 180 - alfa_grad
+    if x_stop > x_center:
+        if alfa_grad >= 0:
+            alfa_start = 360 - alfa_grad
+            return alfa_start
+        alfa_start = 180 + alfa_grad
+    return alfa_start
+
+
+# # dont use now
+# def radius_flow(A, yc, xc, r, alfa):
+#     rend_x = xc + r * math.sin(math.radians(alfa))
+#     rend_y = yc + r * math.cos(math.radians(alfa))
+#     x_stop, y_stop = end_of_line(A, xc, yc, rend_x, rend_y, 0)
+#     x_stop = math.trunc(x_stop)
+#     y_stop = math.trunc(y_stop)
+#     return x_stop, y_stop
+
+
+def check_forward(x, y, DIR, C_map):
+    x_check = x + DIR[0]
+    y_check = y + DIR[1]
+    barier = C_map[y_check][x_check]
+    return barier
+
+
+def check_left(x, y, DIR, direction, C_map):
+    barier = 0
+    if DIR == 'Left':
+        barier = C_map[y + direction.get('Down')[0]][x + direction.get('Down')[1]]
+    if DIR == 'Right':
+        barier = C_map[y + direction.get('Up')[0]][x + direction.get('Up')[1]]
+    if DIR == 'Up':
+        barier = C_map[y + direction.get('Left')[0]][x + direction.get('Left')[1]]
+    if DIR == 'Down':
+        barier = C_map[y + direction.get('Right')[0]][x + direction.get('Right')[1]]
+    return barier
+
+
+def check_back_left(x, y, DIR, direction_, C_map):
+    barier = 0
+    if DIR == 'Left':
+        barier = C_map[y + direction_.get('dr')[0]][x + direction_.get('dr')[1]]
+    if DIR == 'Right':
+        barier = C_map[y + direction_.get('ul')[0]][x + direction_.get('ul')[1]]
+    if DIR == 'Up':
+        barier = C_map[y + direction_.get('dl')[0]][x + direction_.get('dl')[1]]
+    if DIR == 'Down':
+        barier = C_map[y + direction_.get('ur')[0]][x + direction_.get('ur')[1]]
+    return barier
+
+
+def check_right(x, y, DIR, direction, C_map):
+    barier = 0
+    if DIR == 'Left':
+        barier = C_map[y + direction.get('Up')[0]][x + direction.get('Up')[1]]
+    if DIR == 'Right':
+        barier = C_map[y + direction.get('Down')[0]][x + direction.get('Down')[1]]
+    if DIR == 'Up':
+        barier = C_map[y + direction.get('Right')[0]][x + direction.get('Right')[1]]
+    if DIR == 'Down':
+        barier = C_map[y + direction.get('Left')[0]][x + direction.get('Left')[1]]
+    return barier
+
+
+def check_back_right(x, y, DIR, direction_, C_map):
+    barier = 0
+    if DIR == 'Left':
+        barier = C_map[y + direction_.get('ur')[0]][x + direction_.get('ur')[1]]
+    if DIR == 'Right':
+        barier = C_map[y + direction_.get('dl')[0]][x + direction_.get('dl')[1]]
+    if DIR == 'Up':
+        barier = C_map[y + direction_.get('dr')[0]][x + direction_.get('dr')[1]]
+    if DIR == 'Down':
+        barier = C_map[y + direction_.get('ul')[0]][x + direction_.get('ul')[1]]
+    return barier
+
+
+def step_forward(x, y, DIR):
+    x = x + DIR[0]
+    y = y + DIR[1]
+    return x, y
+
+
+def turn_left(DIR, direction):
+    if DIR == 'Left':
+        DIR = direction.get('Down')
+    if DIR == 'Right':
+        DIR = direction.get('Up')
+    if DIR == 'Up':
+        DIR = direction.get('Left')
+    if DIR == 'Down':
+        DIR = direction.get('Right')
+    return DIR
+
+
+def turn_right(DIR, direction):
+    if DIR == 'Left':
+        DIR = direction.get('Up')
+    if DIR == 'Right':
+        DIR = direction.get('Down')
+    if DIR == 'Up':
+        DIR = direction.get('Right')
+    if DIR == 'Down':
+        DIR = direction.get('Left')
+    return DIR
+
+
+def dist_cell(x_start, y_start, x_finish, y_finish):
+    distance = math.sqrt((x_finish - x_start) * (x_finish - x_start) + (y_finish - y_start) * (y_finish - y_start))
+    return distance
+
+
+#   функция убрана из оборота тк несовершенна для широкого спектра препятствий
+#   подходит только для небольших кругов
+#   подфункции незакомментированы
+# def around_obstacle(B, xf, yf, x_stop, y_stop):  # add safe Radius
+#     C = []
+#     xc, yc = find_center(B)
+#     r = find_rad_obtacle(xc, yc, B)
+#     alfa_start = find_alfa(x_stop, y_stop, xc, yc)
+#     for alfa in range(alfa_start, 360 + alfa_start, 5):
+#         x_new, y_new = radius_flow(A, yc, xc, r, alfa)
+#         xf_try, yf_try = end_of_line(A, x_new, y_new, xf, yf)
+#         if B[yf_try][xf_try] == 0:
+#             C.append(x_new)
+#             C.append(y_new)
+#     for alfa in range(alfa_start, -360 - alfa_start, -5):
+#         x_new, y_new = radius_flow(A, yc, xc, r, alfa)
+#         xf_try, yf_try = end_of_line(A, x_new, y_new, xf, yf)
+#         if B[yf_try][xf_try] == 0:
+#             C.append(x_new)
+#             C.append(y_new)
+#     return C
+
+
+def filter_start_pos(x_start, y_start, direction, direction_, B_map):
+    if B_map[x_start + direction.get('Up')[0]][y_start + direction.get('Up')[1]] and B_map[x_start + direction.get('Down')[0]][
+        y_start + direction.get('Down')[1]] and B_map[x_start + direction.get('Right')[0]][
+        y_start + direction.get('Right')[1]] and B_map[
+        x_start + direction.get('Left')[0]][y_start + direction.get('Left')[1]]:
+        for i in direction_.keys():
+            if B_map[y_start + direction_.get(i)[0]][y_start + direction_.get(i)[1]] == 0:
+                y_start = y_start + direction_.get(i)[0]
+                x_start = y_start + direction_.get(i)[1]
+                return x_start, y_start
+
+    if not B_map[x_start + direction.get('Up')[0]][y_start + direction.get('Up')[1]] and not \
+            B_map[x_start + direction.get('Down')[0]][
+                y_start + direction.get('Down')[1]] and not B_map[x_start + direction.get('Right')[0]][
+        y_start + direction.get('Right')[1]] and not B_map[
+        x_start + direction.get('Left')[0]][y_start + direction.get('Left')[1]]:
+        for i in direction_.keys():
+            if B_map[y_start + direction_.get(i)[0]][y_start + direction_.get(i)[1]] == 1:
+                y_start = y_start + direction_.get(i)[0]
+                x_start = y_start + direction_.get(i)[1]
+                return x_start, y_start
+    return x_start, y_start
+
+
+def go_along_wall(A, x_start, y_start, x_finish, y_finish, side):
+    x, y = x_start, y_start
+    xff, yff = x_start, y_start
+    direction = {'Up': [-1, 0], 'Down': [1, 0], 'Left': [0, -1], 'Right': [0, 1]}
+    direction_ = {'ul': [-1, -1], 'ur': [-1, 1], 'dr': [1, 1], 'dl': [1, -1]}
+    x, y = filter_start_pos(x, y, direction, direction_, A)
+    DIR = direction.get('Left')
+    if side == 'Left':
+        while dist_cell(x_finish, y_finish, xff, yff) < 2:
+            if check_forward(x, y, DIR, A) == 1 and check_left(x, y, DIR, direction, A) == 1:
+                turn_right(DIR, direction)
+            if check_left(x, y, DIR, direction, A) == 0:
+                if check_back_left(x, y, DIR, direction_, A) == 1:
+                    turn_left(DIR, direction)
+                    x, y = step_forward(x, y, DIR)
+                    continue
+                turn_left(DIR, direction)
+            if check_forward(x, y, DIR, A) == 0 and check_left(x, y, DIR, direction, A) == 1:
+                x, y = step_forward(x, y, DIR)
+            xff, yff = end_of_line(A, x, y, x_finish, y_finish)
+        return x, y
+    if side == 'Right':
+        while dist_cell(x, y, xff, yff) < 2:
+            if check_forward(x, y, DIR, A) == 1 and check_right(x, y, DIR, direction, A) == 1:
+                turn_left(DIR, direction)
+            if check_right(x, y, DIR, direction, A) == 0:
+                if check_back_right(x, y, DIR, direction_, A) == 1:
+                    turn_right(DIR, direction)
+                    x, y = step_forward(x, y, DIR)
+                    continue
+                turn_right(DIR, direction)
+            if check_forward(x, y, DIR, A) == 0 and check_right(x, y, DIR, direction, A) == 1:
+                x, y = step_forward(x, y, DIR)
+            xff, yff = end_of_line(A, x, y, x_finish, y_finish)
+        return x, y
+    # дописать остановку или масштабирование при конце фрагмента карты
+    # дописать заплатку обхода при наступлении на рамку  !!!!!!
+    # дописать заплатку упора в другой объект  !!!!!!
+
+
+def map_sliser_deep_bubble(A_map, deep, bubble):
+    B_map = create_zero_matrix(len(A_map[0]), len(A))
+    for stroka in range(len(B_map)):
+        for number in range(len(B_map[stroka])):
+            if A_map[stroka][number] < deep:
+                B_map[stroka][number] = 1
+    for i in range(bubble):
+        B_map = create_1st_S(B_map)
+    return B_map
+
+
+def is_obtacle_touch_frame(C_map, s=1):
+    for elem in C_map[0]:
+        if elem == s:
+            return 1
+    for elem in C_map[-1]:
+        if elem == s:
+            return 1
+    for elem in range(1, len(C_map) - 1):
+        if C_map[elem][0] == s or C_map[elem][-1] == s:
+            return 1
+    return 0
+
+
+def type_of_obtacle(B_map, x_stop, y_stop):
+    obt_type = '0'
+    C_map = create_filled_contour(x_stop, y_stop, B_map)  # add check outsides in contour
+    if is_obtacle_touch_frame(C_map):
+        obt_type = "coast"
+        return obt_type
+    obt_type = "island"
+    return obt_type, C_map
+
+
+def start_to_finish(B, xs, ys, xf, yf):
+    xff, yff = -1, -1
+    while xff != xf and yff != yf:
+        xff, yff = end_of_line(B, xs, ys, xf, yf)
+        obt_type, C_map = type_of_obtacle(B_map, x_stop, y_stop)
+        # B = create_1st_S(B)
+        if obt_type == "coast":
+            go_along_wall(A, x_start, y_start, x_finish, y_finish, side)
+            go_along_wall(A, x_start, y_start, x_finish, y_finish, side)
+        if obt_type == "island":
+            go_along_wall(A, x_start, y_start, x_finish, y_finish, side)
+            go_along_wall(A, x_start, y_start, x_finish, y_finish, side)
+
+        f = Node("F")
+        w_graph = Graph.create_from_nodes([a, b, c, d, e, f])
+        w_graph.connect(a, b, 5)
+    return
---
+++ searcher.py
@@ -0,0 +1,96 @@
+import math
+
+
+def create_zero(x, y):
+    A = [[0] * x for n in range(y)]
+    return A
+
+
+def where_unknown(A, B, error):
+    F = create_zero(len(A[1]), len(A))
+    for y in range(len(A)):
+        for x in range(len(A[y])):
+            if A[y][x] == B[0]:
+                for alfa in range(0, 360, 45):
+                    deep, x_now, y_now = trans_sys(A, y, x, 1, alfa)
+                    if B[1] <= (deep + error) and (B[1] >= deep - error):
+                        y_now, x_now = contine_side(A, B, y, x, alfa, error)
+                        if y_now != 0 and x_now != 0:
+                            F[to_int(y_now)][to_int(x_now)] = 1
+    return F
+
+
+def where_your_know(A, B, alfa, error):
+    F = create_zero(len(A[1]), len(A))
+    for y in range(len(A)):
+        for x in range(len(A[y])):
+            if A[y][x] == B[0]:
+                deep, x_now, y_now = trans_sys(A, y, x, 1, alfa)
+                if B[1] <= (deep + error) and (B[1] >= deep - error):
+                    y_now, x_now = contine_side(A, B, y, x, alfa, error)
+                    if y_now != 0 and x_now != 0:
+                        F[to_int(y_now)][to_int(x_now)] = 1
+    return F
+
+
+def contine_side(A, B, y, x, alfa, error):
+    x_now = x
+    y_now = y
+    for i in range(2, len(B)):
+        deep, x_now, y_now = trans_sys(A, y, x, i, alfa)
+        if B[i] <= (deep + error) and (B[i] >= deep - error):
+            continue
+        if B[i] > (deep + error) or (B[i] < deep - error):
+            return 0, 0
+        #####
+        # для создания памяти направления для сложной навигации сюда придется записывать угол
+    return (y_now, x_now)
+
+
+def expand_matrix(A):
+    B = create_zero(len(A) * 2, len(A[0]) * 2)
+    ######  nodes
+    for y in range(len(A)):
+        for x in range(len(A[y])):
+            B[y * 2][x * 2] = A[y][x]
+    ####  1st stroka
+    for y in range(0, len(B), 2):
+        for x in range(1, len(B[y]) - 1, 2):
+            B[y][x] = ((B[y][x - 1] + B[y][x + 1]) / 2)
+    #####  1st stolbik
+    for y in range(1, len(B) - 1, 2):
+        for x in range(0, len(B[y]), 2):
+            B[y][x] = ((B[y - 1][x] + B[y + 1][x]) / 2)
+    ####3 center
+    for y in range(1, len(B) - 1, 2):
+        for x in range(1, len(B[y]) - 1, 2):
+            B[y][x] = (((B[y][x - 1] + B[y][x + 1]) / 2) + ((B[y - 1][x] + B[y + 1][x]) / 2)) / 2
+    return B
+
+
+def trans_sys(A, y, x, i, alfa):
+    add_pos_x = i * math.sin(math.radians(alfa))
+    add_pos_y = i * math.cos(math.radians(alfa))
+    x_real = x + add_pos_x
+    y_real = y + add_pos_y
+    x_min = math.floor(x_real)
+    x_max = math.ceil(x_real)
+    y_min = math.floor(y_real)
+    y_max = math.ceil(y_real)
+    about_x = x_real - x_min
+    about_y = y_real - y_min
+    delta_deep_xU = A[y_min][x_max] - A[y_min][x_min]
+    delta_deep_xD = A[y_max][x_max] - A[y_max][x_min]
+    deep_xU = A[y_min][x_min] + delta_deep_xU * about_x
+    deep_xD = A[y_max][x_min] + delta_deep_xD * about_x
+    delta_deep_yC = deep_xD - deep_xU
+    deep = deep_xU + delta_deep_yC * about_y
+    return deep, x_real, y_real
+
+
+def to_int(z):
+    if (z - math.floor(z)) < (math.ceil(z) - z):
+        z = math.floor(z)
+    else:
+        z = math.ceil(z)
+    return z
